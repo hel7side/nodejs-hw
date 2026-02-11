3759d19dbe0d4189b1ab691bf29a6fae7c590027
hel7side <he11sidefn@gmail.com>
2026-02-11 19:20:04 +0100

    02-mongodb: add mongoose and refactor structure

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
@@ -40,5 +41,57 @@ await connectMongoDB();
 
 //Запуск сервера
 app.listen(PORT, ()=>{
+=======
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+>>>>>>> 1db70adf9d5df61787d10a045370e044376301f7
   console.log(`Server is running on port ${PORT}`);
 });
