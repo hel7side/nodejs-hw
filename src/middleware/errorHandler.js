1e2ebba9f3eb9baa35e203a8fa030427542bad02
hel7side <he11sidefn@gmail.com>
2026-02-11 21:29:48 +0100

    fixx

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,16 +1,16 @@
 // src/middleware/errorHandler.js
 
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  // Check if error is created by http-errors
-  if (err instanceof createHttpError.HttpError) {
+  // Check if error is an instance of HttpError
+  if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message,
     });
   }
 
-  // Handle other errors
+  // Handle other (unexpected) errors
   const isProd = process.env.NODE_ENV === 'production';
 
   return res.status(500).json({
